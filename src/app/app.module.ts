ea7c926776ec24028e882932ab0197c2f134d27c
AquilesDias <kilesax@gmail.com>
2023-02-23 11:16:56 -0300

    adicionando imports que serao utilizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatCardModule } from '@angular/material/card'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatInputModule } from '@angular/material/input'; 
+import { MatListModule } from '@angular/material/list'; 
+import { MatPaginatorModule } from '@angular/material/paginator'; 
+import { MatRadioModule } from '@angular/material/radio'; 
+import { MatSelectModule } from '@angular/material/select'; 
+import { MatSidenavModule } from '@angular/material/sidenav'; 
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';  
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +26,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
