ac534788e69a91fe469a68b8d1e61f1e16a3d536
AquilesDias <kilesax@gmail.com>
2023-02-28 11:44:47 -0300

    Criando html lista de tecnico e entidade

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Tecnico } from './../../../models/tecnico';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-tecnico-list',
   templateUrl: './tecnico-list.component.html',
   styleUrls: ['./tecnico-list.component.css']
 })
+
 export class TecnicoListComponent implements OnInit {
+
+  ELEMENT_DATA : Tecnico [] = [
+    {
+      id:1,
+      nome: 'Naruto',
+      cpf: '00442221112',
+      email: 'naruto@gmail',
+      senha: '1234',
+      perfis: [],
+      dataCriacao: 23/12/12     
+    }
+  ]
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
+
